e5bf4fc61ffcb658bacc74fce33997af0d2463be
Jonathan Kilgallon <26605451+KilgallonJ@users.noreply.github.com>
2018-05-30 15:12:49 +0100

    Removed company copyright statement

--- HibernateTest/src/main/java/hibernatetest/persistence/HibernateTestDAO.java
+++ HibernateTest/src/main/java/hibernatetest/persistence/HibernateTestDAO.java
@@ -1,9 +1,3 @@
-/**
- * (c) Midland Software Limited 2018
- * Name     : HibernateTestDAO.java
- * Author   : kilgallonj
- * Date     : 23 May 2018
- */
 package hibernatetest.persistence;
 
 import javax.persistence.EntityManager;
